d6841c732e24d254877406dc846f286dfcdf089f
Omar Chadidi <omar.chadidi@hi5group.co.uk>
2021-11-26 18:59:18 +0100

    Create new a custom hook useFetch for api calls

---
+++ util/hooks.ts
@@ -0,0 +1,56 @@
+import { useReducer } from "react";
+import http from "../Http/http";
+
+interface State<T> {
+  data?: T;
+  error?: Error;
+}
+
+// discriminated union type
+type Action<T> =
+  | { type: "loading" }
+  | { type: "fetched"; payload: T }
+  | { type: "error"; payload: Error };
+
+export function useFetch<T = unknown>(endpoint: string): [State<T>, Function] {
+  const initialState: State<T> = {
+    error: undefined,
+    data: undefined,
+  };
+
+  // Keep state logic separated
+  const fetchReducer = (state: State<T>, action: Action<T>): State<T> => {
+    switch (action.type) {
+      case "loading":
+        return { ...initialState };
+      case "fetched":
+        return { ...initialState, data: action.payload };
+      case "error":
+        return { ...initialState, error: action.payload };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+  const fetchData = async (query: string) => {
+    if (!endpoint) return;
+
+    dispatch({ type: "loading" });
+
+    try {
+      const { data, statusText } = await http.get(`${endpoint}?${query}`);
+
+      if (statusText !== "OK") {
+        throw new Error(statusText);
+      }
+
+      dispatch({ type: "fetched", payload: data });
+    } catch (error) {
+      dispatch({ type: "error", payload: error as Error });
+    }
+  };
+
+  return [state, fetchData];
+}
